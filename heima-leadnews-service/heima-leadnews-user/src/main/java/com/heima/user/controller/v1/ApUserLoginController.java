1c24627c08afe02044457274aec2634c633a261f
StreamAzure <1905105036@qq.com>
2024-01-14 14:53:22 +0800

    gateway 服务配置

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -5,6 +5,7 @@ import com.heima.model.user.dtos.LoginDto;
 import com.heima.user.service.ApUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/login")
 @Api(value = "App端用户登录", tags = "ap_user")
+@Slf4j
 public class ApUserLoginController {
     @Autowired
     private ApUserService apUserService;
@@ -21,6 +23,7 @@ public class ApUserLoginController {
     @ApiOperation("用户登录")
     @PostMapping("/login_auth")
     public ResponseResult login(@RequestBody LoginDto dto) {
+        log.info("/login_auth: {}", dto);
         return apUserService.login(dto);
     }
 }

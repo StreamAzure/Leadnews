f9db26c4ee87524bf7f7997b5f64943e3b55bb90
StreamAzure <1905105036@qq.com>
2024-01-15 15:45:29 +0800

    自媒体素材管理-素材列表查看（前端有点问题加载不出预览图）

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
@@ -1,15 +1,19 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
 import com.heima.wemedia.service.WmMaterialService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/material")
+@Slf4j
 public class WmMaterialController {
 
     @Autowired
@@ -18,7 +22,14 @@ public class WmMaterialController {
 
     @PostMapping("/upload_picture")
     public ResponseResult uploadPicture(MultipartFile multipartFile){
+        log.info("uploadPictrue: {}", multipartFile);
         return wmMaterialService.uploadPicture(multipartFile);
+    }
+
+    @PostMapping("/list")
+    public ResponseResult findList(@RequestBody WmMaterialDto dto){
+        log.info("findList: {}", dto);
+        return wmMaterialService.findList(dto);
     }
 
 }

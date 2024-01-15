b68638f4c2e563612f97b9e2bcf9657ebcebfb9a
StreamAzure <1905105036@qq.com>
2024-01-15 21:15:27 +0800

    自媒体文章-文章保存Feign接口

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,9 +1,11 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
@@ -15,6 +17,13 @@ public interface ApArticleService extends IService<ApArticle> {
      * @param dto
      * @return
      */
-    ResponseResult load(Short loadtype, ArticleHomeDto dto);
+    public ResponseResult load(Short loadtype, ArticleHomeDto dto);
+
+    /**
+     * 保存APP端相关文章
+     * @param articleDto
+     * @return
+     */
+    public ResponseResult saveArticle(@RequestBody ArticleDto articleDto);
 
 }

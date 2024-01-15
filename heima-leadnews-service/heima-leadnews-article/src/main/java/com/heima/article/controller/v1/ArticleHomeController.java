0af9ded866fabc3454981458966df626f4b1147b
StreamAzure <1905105036@qq.com>
2024-01-15 11:30:37 +0800

    ArticleController添加日志记录；改了下服务端口

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -4,6 +4,7 @@ import com.heima.article.service.ApArticleService;
 import com.heima.common.constants.ArticleConstants;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.common.dtos.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,24 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/article")
+@Slf4j
 public class ArticleHomeController {
-
-
     @Autowired
     private ApArticleService apArticleService;
 
     @PostMapping("/load")
     public ResponseResult load(@RequestBody ArticleHomeDto dto) {
+        log.info("Article load: {}", dto);
         return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_MORE,dto);
     }
 
     @PostMapping("/loadmore")
     public ResponseResult loadMore(@RequestBody ArticleHomeDto dto) {
+        log.info("Article load more: {}", dto);
         return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_MORE,dto);
     }
 
     @PostMapping("/loadnew")
     public ResponseResult loadNew(@RequestBody ArticleHomeDto dto) {
+        log.info("Article loadnew: {}", dto);
         return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_NEW,dto);
     }
 }

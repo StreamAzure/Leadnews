967682f92de371aa4e4ca696a3f2c5b10c430d66
StreamAzure <1905105036@qq.com>
2024-02-02 15:13:03 +0800

    自媒体文章-审核文章增加自定义敏感词过滤（不启用）

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
@@ -1,6 +1,7 @@
 package com.heima.wemedia.service.impl;
 
 import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.heima.apis.article.IArticleClient;
 import com.heima.common.aliyun.GreenImageScan;
 import com.heima.common.aliyun.GreenTextScan;
@@ -9,9 +10,12 @@ import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.wemedia.pojos.WmChannel;
 import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.model.wemedia.pojos.WmSensitive;
 import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.utils.common.SensitiveWordUtil;
 import com.heima.wemedia.mapper.WmChannelMapper;
 import com.heima.wemedia.mapper.WmNewsMapper;
+import com.heima.wemedia.mapper.WmSensitiveMapper;
 import com.heima.wemedia.mapper.WmUserMapper;
 import com.heima.wemedia.service.WmNewsAutoScanService;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +27,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Wrapper;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -52,6 +57,9 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
         if(wmNews.getStatus().equals(WmNews.Status.SUBMIT.getCode())){
 //            //从内容中提取纯文本内容和图片
 //            Map<String,Object> textAndImages = handleTextAndImages(wmNews);
+
+            // 自定义敏感词过滤
+//            boolean isSensitive = handleSensitiveScan((String) textAndImages.get("content"), wmNews);
 //
 //            //2.审核文本内容  阿里云接口
 //            boolean isTextScan = handleTextScan((String) textAndImages.get("content"),wmNews);
@@ -71,6 +79,26 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
             wmNews.setArticleId((Long) responseResult.getData());
             updateWmNews(wmNews,(short) 9,"审核成功");
         }
+    }
+
+    @Autowired
+    private WmSensitiveMapper wmSensitiveMapper;
+    private boolean handleSensitiveScan(String content, WmNews wmNews) {
+
+        boolean flag = true;
+
+        // 获取所有敏感词
+        List<WmSensitive> wmSensitives = wmSensitiveMapper.selectList(Wrappers.<WmSensitive>lambdaQuery().select(WmSensitive::getSensitives));
+        List<String> sensitiveList = wmSensitives.stream().map(WmSensitive::getSensitives).collect(Collectors.toList());
+        // 初始化敏感词库
+        SensitiveWordUtil.initMap(sensitiveList);
+        // 查看文章中是否包含敏感词
+        Map<String, Integer> map = SensitiveWordUtil.matchWords(content);
+        if(map.size() > 0){
+            updateWmNews(wmNews, (short) 2, "当前文章中存在违规内容：" + map);
+            flag = false;
+        }
+        return flag;
     }
 
     @Qualifier("com.heima.apis.article.IArticleClient")

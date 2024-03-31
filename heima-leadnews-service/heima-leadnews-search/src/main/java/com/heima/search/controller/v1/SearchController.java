49c5d3b9b1c668829344870f8db4a8113feded22
StreamAzure <1905105036@qq.com>
2024-03-31 15:54:42 +0800

    APP文章搜索-实现文章搜索结果呈现&关键词高亮显示

---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/SearchController.java
@@ -0,0 +1,30 @@
+package com.heima.search.controller.v1;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.UserSearchDto;
+import com.heima.search.service.ArticleSearchService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/article/search")
+@Slf4j
+public class SearchController {
+
+    @Autowired
+    private ArticleSearchService articleSearchService;
+    @PostMapping("/search")
+    public ResponseResult search(@RequestBody UserSearchDto userSearchDto){
+        try {
+            return articleSearchService.search(userSearchDto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

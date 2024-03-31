ee3eb4ac578cfee597388009df1652070e6a8d71
StreamAzure <1905105036@qq.com>
2024-03-31 18:35:04 +0800

    APP搜索历史记录-Search服务新增拦截器拿到HTTP报头中的UserId并放入ThreadLocal

---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
@@ -0,0 +1,47 @@
+package com.heima.search.interceptor;
+
+import com.heima.model.user.pojos.ApUser;
+import com.heima.utils.thread.AppThreadLocalUtils;
+import com.heima.utils.thread.WmThreadLocalUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+public class AppTokenInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //得到header中的信息
+        String userId = request.getHeader("userId");
+        Optional<String> optional = Optional.ofNullable(userId);
+        if(optional.isPresent()){
+            //把用户id存入threadloacl中
+            ApUser apUser = new ApUser();
+            apUser.setId(Integer.valueOf(userId));
+            AppThreadLocalUtils.setUser(apUser);
+//            log.info("wmTokenFilter设置用户信息到threadlocal中...");
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        log.info("清理threadlocal...");
+        AppThreadLocalUtils.clear();
+    }
+
+    /**
+     * 确保即使抛出异常，ThreadLocal内的数据仍会被清理
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex){
+        AppThreadLocalUtils.clear();
+    }
+}
